ccf8e3447fbad954c76719eefe77cbe9b36ab59c
Jason <jasontang20043@gmail.com>
2024-10-08 01:54:04 -0700

    setup controllers to get meets from db

---
+++ server/controllers/meets.js
@@ -0,0 +1,38 @@
+import { pool } from "./database.js";
+
+const getMeets = async (req, res) => {
+  try {
+    const meets = await pool.query(`SELECT * FROM meets GROUP BY state`)
+    res.status(200).json(meets.rows)
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
+
+const getMeetsByState = async (req, res) => {
+  try {
+    const selectQuery = `SELECT * FROM meets WHERE state = $1`
+    const state = [req.params.state]
+    const results = await pool.query(selectQuery, state)
+    res.status(200).json(results.rows)
+  } catch (error) { 
+    res.status(409).json({ message: error.message })
+  }
+}
+
+const getNumberOfMeetsForState = async (req, res) => {
+  try {
+    const countQuery = `SELECT COUNT(*) FROM meets WHERE state = $1`
+    const values = [req.params.state] 
+    const results = await pool.query(countQuery, values)
+    res.status(200).json(results.rows[0])
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
+
+export {
+  getMeets,
+  getMeetsByState,
+  getNumberOfMeetsForState
+}
